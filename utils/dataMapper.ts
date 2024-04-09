4b12e83018835fd54c4166c490f025eeb4c71206
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-09 22:11:32 +0200

    feat: add function to extract ingredients from api data

--- utils/dataMapper.ts
+++ utils/dataMapper.ts
@@ -1,4 +1,10 @@
-export const getAllergensFromData = (data: any): any => {
-  console.log(Object.keys(data.product));
+export const getAllergens = (data: any): any => {
+  //console.log(Object.keys(data.product));
   return data.product['allergens_from_ingredients'];
+};
+
+export const getIngredients = (data: any): string[] => {
+  const ingredients = data.product.ingredients;
+
+  return ingredients.map((ingredient: any) => ingredient.text);
 };
