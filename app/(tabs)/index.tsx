a5d1ccbbc05d0764ae088d9b623dba21b3d3cbac
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-09 22:11:11 +0200

    feat: display fetched ingredients in list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,42 @@
-import { Pressable, StyleSheet } from 'react-native';
+import { FlatList, Pressable, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { Link } from 'expo-router';
-import { useBearStore } from '@/hooks/useBarcodeStore';
-import { getAllergensFromData } from '@/utils/dataMapper';
+import { useBarcodeStore } from '@/hooks/useBarcodeStore';
+import { getAllergens, getIngredients } from '@/utils/dataMapper';
 import { useEffect, useState } from 'react';
 
 export default function TabOneScreen() {
-  const [allergens, setAllergens] = useState();
-  const { barcode } = useBearStore();
+  const [ingredients, setIngredients] = useState<string[]>();
+  const { barcode } = useBarcodeStore();
 
   useEffect(() => {
     if (!barcode) return;
     const getFoodData = async () => {
       const result = await fetch(`https://world.openfoodfacts.org/api/v2/product/${barcode}.json`);
       const foodData = await result.json();
-      const allergens = getAllergensFromData(foodData);
-      setAllergens(allergens);
+      setIngredients(getIngredients(foodData));
     };
     getFoodData();
   }, [barcode]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Tab One</Text>
+      </View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>{barcode}</Text>
-      <Text>This product has following allergens {allergens}</Text>
-      <Link href="/CameraModal" asChild>
-        <Pressable>
-          <Text>Open Camera Modal</Text>
-        </Pressable>
-      </Link>
+      <View style={styles.ingredientContainer}>
+        <Link href="/CameraModal" asChild>
+          <Pressable style={styles.cameraButton}>
+            <Text>Open Barcode Scanner</Text>
+          </Pressable>
+        </Link>
+        <Text>This is the barcode: {barcode}</Text>
+        <Text>This product has the following ingredients:</Text>
+        <FlatList data={ingredients} renderItem={({ item }) => <Text>{item}</Text>} />
+      </View>
     </View>
   );
 }
@@ -42,14 +46,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 16,
+  },
+  titleContainer: {
+    flex: 1,
+    marginTop: 32,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: '80%',
+  },
+  cameraButton: {
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    width: '80%',
+  },
+  ingredientContainer: {
+    flex: 2,
+    gap: 8,
+    alignItems: 'center',
   },
 });
