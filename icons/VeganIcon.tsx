72f2a107fa572e8292113a9ea95a5d1ec0cbb153
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-12 08:58:48 +0200

    feat: add icons for animal product information

---
+++ icons/VeganIcon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Svg, { Circle, Path, SvgProps } from 'react-native-svg';
+// made by (inspired by) edent
+export const VeganIcon = (props: SvgProps) => (
+  <Svg aria-label="Vegetarian" role="img" viewBox="0 0 512 512" {...props}>
+    <Circle cx={256} cy={256} r={220} stroke="#0f0" fill={'white'} strokeWidth={25} />
+    <Path fill="#0f0" d="m140 120h44l72 225 72-225h44l-90 265h-52z" />
+  </Svg>
+);
