e4e3e7a363f1960dd177a4d127207df0d3401612
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-12 08:59:25 +0200

    refactor: use internal styledview instead of reactnative view

--- components/ScoreDisplay.tsx
+++ components/ScoreDisplay.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View, Text } from '@/components/Themed';
 
 export type ScoreDisplayProps = {
   score: number;
