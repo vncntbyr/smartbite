3227b1c8e433443bd8aed8066f31734964c8d162
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-12 08:56:30 +0200

    refactor: rename template files to camel case

--- app/InfoModal.tsx
+++ app/InfoModal.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function ModalScreen() {
@@ -9,8 +8,8 @@ export default function ModalScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
+      {/* USE THIS FOR OPEN WEBVIEW TO OPENFOODFACTS
+       WebBrowser.openBrowserAsync(props.href as string); */}
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
