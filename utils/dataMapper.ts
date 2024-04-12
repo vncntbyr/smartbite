e8049f62152bb065a0a7cc13ace847379cc51bfd
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-12 08:59:00 +0200

    feat: add more data extraction methods

--- utils/dataMapper.ts
+++ utils/dataMapper.ts
@@ -3,8 +3,59 @@ export const getAllergens = (data: any): any => {
   return data.product['allergens_from_ingredients'];
 };
 
+// TODO: This is a mess and does not work, clean up!
 export const getIngredients = (data: any): string[] => {
+  const checkIfNested = (ingredient: any) => {
+    const mapNestedIngredients = (ingredient: any) => {
+      return ingredient.map((ingredient: any) => checkIfNested(ingredient));
+    };
+    return Array.isArray(ingredient.ingredient)
+      ? mapNestedIngredients(ingredient.ingredient)
+      : removeUnderscores(ingredient.text);
+  };
+  const removeUnderscores = (ingredient: string) => {
+    return ingredient.replace(/_/g, '');
+  };
   const ingredients = data.product.ingredients;
 
-  return ingredients.map((ingredient: any) => ingredient.text);
+  return ingredients.map((ingredient: any) => checkIfNested(ingredient));
+};
+
+export const getImgUrl = (data: any): string => {
+  return data.product.image_url;
+};
+
+export const getNutrients = (data: any): any => {
+  const {
+    carbohydrates,
+    fat,
+    proteins,
+    'energy-kcal': energyKcal,
+    'energy-kcal_unit': energyKcalUnit,
+    sugars,
+    salt,
+  } = data.product.nutriments;
+  return {
+    carbohydrates,
+    fat,
+    proteins,
+    energyKcal,
+    energyKcalUnit,
+    sugars,
+    salt,
+  };
+};
+
+export const getScores = (data: any): any => {
+  const novaScore = data.product.nova_group;
+  const { grade, negative_points, positive_points, score } = data.product.nutriscore_data;
+  const ecoScoreData = data.product.ecoscore_data;
+  return {
+    novaScore,
+    grade,
+    negativePoints: negative_points,
+    positivePoints: positive_points,
+    score,
+    ecoScoreData,
+  };
 };
