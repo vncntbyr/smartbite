e731d5d1761dd6fd38234fee012fef21757c476e
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-13 11:17:03 +0200

    feat: pass down mapped values to components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,33 +3,43 @@ import { FlatList, Pressable, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { Link } from 'expo-router';
 import { useBarcodeStore } from '@/hooks/useBarcodeStore';
-import { getImgUrl, getIngredients, getNutrients } from '@/utils/dataMapper';
+import { getImgUrl, getIngredients, getNutrients, getScores } from '@/utils/dataMapper';
 import { ProductOverview } from '@/components/ProductOverview';
 import { IngredientItem } from '@/components/IngredientItem';
+import type { Scores } from '@/types/scores';
+import type { Ingredient } from '@/types/Ingredient';
 
 export default function TabOneScreen() {
-  const [ingredients, setIngredients] = useState<string[]>();
+  const [ingredients, setIngredients] = useState<Ingredient[]>();
   const [imgUrl, setImgUrl] = useState<string>(''); // TODO: handle what happens when there is no image url
   const [nutrients, setNutrients] = useState<Record<string, number>>({}); // TODO: handle what happens when there is no image url
   const [productName, setProductName] = useState<string>(''); // TODO: handle what happens when there is no image url
+  const [scores, setScores] = useState<Scores>(); // TODO: handle what happens when there is no image url
   // const { barcode } = useBarcodeStore();
-  const barcode = '4008258154229';
+  // Studentenfutter 4008258154229, Nudelsuppe 737628064502, Salami 20036362
+  const barcode = '20036362';
   useEffect(() => {
     if (!barcode) return;
     const getFoodData = async () => {
-      const result = await fetch(`https://world.openfoodfacts.org/api/v2/product/${barcode}.json`);
+      const result = await fetch(`https://world.openfoodfacts.org/api/v3/product/${barcode}.json`);
       const foodData = await result.json();
       setIngredients(getIngredients(foodData));
       setProductName(foodData.product.product_name);
       setImgUrl(getImgUrl(foodData));
       setNutrients(getNutrients(foodData));
+      setScores(getScores(foodData));
     };
     getFoodData();
   }, [barcode]);
 
   return (
     <View style={styles.container}>
-      <ProductOverview imgUrl={imgUrl} productName={productName} nutrients={nutrients} />
+      <ProductOverview
+        imgUrl={imgUrl}
+        productName={productName}
+        nutrients={nutrients}
+        {...scores}
+      />
       <View style={styles.ingredientContainer}>
         <Text style={styles.title}>Ingredients</Text>
         <FlatList
@@ -37,7 +47,11 @@ export default function TabOneScreen() {
           contentContainerStyle={styles.ingredientContentContainer}
           style={styles.ingredientList}
           renderItem={({ item }) => (
-            <IngredientItem ingredientName={item} style={styles.ingredientList} />
+            <IngredientItem
+              ingredientName={item.name}
+              isVegan={item.isVegan}
+              isVegetarian={item.isVegetarian}
+            />
           )}
         />
       </View>
