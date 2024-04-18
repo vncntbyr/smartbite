6d99e9aea3535bddfa968f26b3ac2c40e554d90e
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-18 20:42:06 +0200

    feat: add function to get shadow color according to color scheme

--- utils/color.ts
+++ utils/color.ts
@@ -1,6 +1,10 @@
 import { Colors, type ColorType } from '@/constants/Colors';
 import { useColorScheme } from 'react-native';
 
-export const getCorrectColor = (color: keyof ColorType) => {
+export const getBackgroundColor = (color: keyof ColorType) => {
   return useColorScheme() === 'light' ? Colors[color]['400'] : Colors[color]['600'];
+};
+
+export const getShadowColor = (color: keyof ColorType) => {
+  return useColorScheme() === 'light' ? Colors[color]['300'] : Colors[color]['400'];
 };
