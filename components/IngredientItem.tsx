250449d5e28a2b57915f0aa987ed39138c8a82b1
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-23 22:13:00 +0200

    refactor: use Bar component for IngredientItem

--- components/IngredientItem.tsx
+++ components/IngredientItem.tsx
@@ -2,9 +2,9 @@ import { VeganIcon } from '@/icons/VeganIcon';
 import { VegetarianIcon } from '@/icons/VegetarianIcon';
 import { StyleSheet } from 'react-native';
 import { View, Text } from '@/components/Themed';
-import { getShadowColor } from '@/utils/color';
 import { MeatIcon } from '@/icons/MeatIcon';
 import { NonVeganIcon } from '@/icons/NonVeganIcon';
+import { Bar } from './Bar';
 
 type IngredientItemProps = {
   ingredientName: string;
@@ -18,15 +18,7 @@ export function IngredientItem({
   isVegetarian = false,
 }: IngredientItemProps): JSX.Element {
   return (
-    <View
-      style={[
-        styles.ingredientBar,
-        {
-          borderColor: getShadowColor('gray'),
-          shadowColor: getShadowColor('gray'),
-        },
-      ]}
-    >
+    <Bar>
       <Text style={styles.ingredientTitle}>{ingredientName}</Text>
       <View style={styles.iconContainer}>
         {isVegan ? <VeganIcon width={32} height={32} /> : <NonVeganIcon width={32} height={32} />}
@@ -36,27 +28,11 @@ export function IngredientItem({
           <MeatIcon width={32} height={32} />
         )}
       </View>
-    </View>
+    </Bar>
   );
 }
 
 const styles = StyleSheet.create({
-  ingredientBar: {
-    flex: 1,
-    paddingHorizontal: 24,
-    height: 64,
-    borderRadius: 8,
-    borderWidth: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowRadius: 2,
-    shadowOpacity: 1,
-  },
   ingredientTitle: {
     fontSize: 16,
   },
