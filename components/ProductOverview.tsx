fd23435222d3b65e77bb55fb4d3dda38cb9f1b54
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-26 09:17:41 +0200

    docs: add todos for open issues

--- components/ProductOverview.tsx
+++ components/ProductOverview.tsx
@@ -17,6 +17,8 @@ type ProductOverviewProps = {
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
+  // TODO: Images are not displayed well at the moment (bottles are cut off) -> think of improvement
+  // TODO: Sometimes there is no nutrition score data -> handle this case
 export const ProductOverview = ({
   imgUrl,
   productName,
