b6201205e34514ccef4158a7fd2b308170ac412f
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-27 15:43:47 +0200

    feat: add store for product data

---
+++ storage/productData.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+import type { ProductData } from '@/types/ProductData';
+
+type ProductStore = {
+  activeProduct: ProductData | undefined;
+  setActiveProduct: (productData: ProductData) => Promise<void>;
+  resetActiveProduct: () => Promise<void>;
+};
+
+export const useProductStore = create<ProductStore>((set) => ({
+  activeProduct: undefined,
+  setActiveProduct: async (productData) => {
+    set({ activeProduct: productData || undefined });
+  },
+  resetActiveProduct: async () => {
+    set({ activeProduct: undefined });
+  },
+}));
