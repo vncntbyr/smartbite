32bf8dfc11c1115f64ceb169e2e5b2c7f57f38aa
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-27 15:45:55 +0200

    feat: enable navigation to product detail page from history

--- components/HistoryItem.tsx
+++ components/HistoryItem.tsx
@@ -3,15 +3,18 @@ import { View, Text } from '@/components/Themed';
 import { Bar } from './Bar';
 import type { HistoryData } from '@/types/History';
 import { Image } from 'expo-image';
+import { BarcodeLinkWrapper } from './BarcodeLinkWrapper';
 
 export function HistoryItem({ name, barcode, thumbnailUrl }: HistoryData): JSX.Element {
   return (
-    <Bar>
-      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
-        <Image source={{ uri: thumbnailUrl }} style={{ width: 32, height: 32 }} />
-        <Text style={styles.productName}>{name}</Text>
-      </View>
-    </Bar>
+    <BarcodeLinkWrapper barcode={barcode}>
+      <Bar>
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+          <Image source={{ uri: thumbnailUrl }} style={{ width: 32, height: 32 }} />
+          <Text style={styles.productName}>{name}</Text>
+        </View>
+      </Bar>
+    </BarcodeLinkWrapper>
   );
 }
 
