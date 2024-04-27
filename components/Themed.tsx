17b94dc7e751d341e83bd98f84a8d67dfb213b0e
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-27 13:45:06 +0200

    feat: add themed scroll view

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -4,7 +4,7 @@
  */
 
 import { Text as DefaultText, View as DefaultView } from 'react-native';
-
+import { ScrollView as DefaultScrollView } from 'react-native-gesture-handler';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from './useColorScheme';
 
@@ -15,6 +15,7 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
+export type ScrollViewProps = ThemeProps & DefaultScrollView['props'];
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -42,4 +43,11 @@ export function View(props: ViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function ScrollView(props: ScrollViewProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <DefaultScrollView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
