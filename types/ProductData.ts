26fea28cd0874bab7a06fa1304801f04ba5a9464
Vincent Bayer <vinci.bayer@gmx.de>
2024-04-28 12:21:55 +0200

    type: enrichen product data type to contain all central information

--- types/ProductData.ts
+++ types/ProductData.ts
@@ -2,8 +2,10 @@ import type { Ingredient } from './Ingredient';
 import type { Scores } from './Scores';
 
 export type ProductData = {
+  barcode: string;
   ingredients: Ingredient[];
   imgUrl: string;
+  thumbnailUrl: string;
   nutrients: any;
   productName: string;
   scores?: Scores;
