b5487f9d9e115d983d81def73838a1425909c34e
Vincent Bayer <vinci.bayer@gmx.de>
2024-05-06 22:05:01 +0200

    feat: prepare about tab for functionality by creating dummy navigation elements

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,11 +1,31 @@
 import { StyleSheet, View as ContainerView } from 'react-native';
 
-import { Text, View } from '@/components/atoms/Themed';
+import { NavigationGroup } from '@/components/about/NavigationGroup';
+import { NavigationBar } from '@/components/about/NavigationBar';
+import { icons } from '@/constants/Icons';
+import { useTranslation } from '@/hooks/useTranslation';
 
 export default function AboutScreen() {
+  const t = useTranslation();
   return (
     <ContainerView style={styles.container}>
-      <Text style={styles.title}>About</Text>
+      <NavigationBar icon={icons.user} shadow>
+        {t('about.aboutMe')}
+      </NavigationBar>
+      <NavigationGroup>
+        <NavigationBar icon={icons.data}>{t('about.dataUsage')}</NavigationBar>
+        <NavigationBar icon={icons.analytics}>{t('about.analytics')}</NavigationBar>
+      </NavigationGroup>
+
+      <NavigationGroup>
+        <NavigationBar icon={icons.terms}>{t('about.terms')}</NavigationBar>
+        <NavigationBar icon={icons.privacy}>{t('about.privacy')}</NavigationBar>
+      </NavigationGroup>
+
+      <NavigationGroup>
+        <NavigationBar icon={icons.contact}>{t('about.contact')}</NavigationBar>
+        <NavigationBar icon={icons.coffee}>{t('about.coffee')}</NavigationBar>
+      </NavigationGroup>
     </ContainerView>
   );
 }
@@ -15,6 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 16,
   },
   title: {
     fontSize: 20,
