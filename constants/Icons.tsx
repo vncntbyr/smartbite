12d188edf45542334a72b0da7266f9c2cf2f3ccb
Vincent Bayer <vinci.bayer@gmx.de>
2024-05-06 22:04:26 +0200

    feat: add more icons

--- constants/Icons.tsx
+++ constants/Icons.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
+import { Feather, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import { MeatIcon } from '@/constants/icons/MeatIcon';
 import { NonVeganIcon } from '@/constants/icons/NonVeganIcon';
 import { VeganIcon } from '@/constants/icons/VeganIcon';
 import { VegetarianIcon } from '@/constants/icons/VegetarianIcon';
-import { FontAwesome5 } from '@expo/vector-icons';
-import React from 'react';
 
 export const icons = {
-  user: <FontAwesome5 name="user-astronaut" size={24} color={'black'} />,
+  user: <FontAwesome name="user" size={24} color="black" />,
+  data: <MaterialIcons name="data-usage" size={24} color="black" />,
+  analytics: <MaterialIcons name="analytics" size={24} color="black" />,
+  contact: <MaterialIcons name="connect-without-contact" size={24} color="black" />,
+  coffee: <FontAwesome name="coffee" size={24} color="black" />,
+  terms: <MaterialIcons name="description" size={24} color="black" />,
+  privacy: <MaterialIcons name="privacy-tip" size={24} color="black" />,
   meatIcon: <MeatIcon width={32} height={32} />,
   vegetarianIcon: <VegetarianIcon width={32} height={32} />,
   veganIcon: <VeganIcon width={32} height={32} />,
