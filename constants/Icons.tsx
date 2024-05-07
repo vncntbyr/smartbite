9dd9bf4452830f87b9c2cefc01f30f7dd77821b4
Vincent Bayer <vinci.bayer@gmx.de>
2024-05-07 23:02:08 +0200

    style: increase icon sizes

--- constants/Icons.tsx
+++ constants/Icons.tsx
@@ -6,13 +6,13 @@ import { VeganIcon } from '@/constants/icons/VeganIcon';
 import { VegetarianIcon } from '@/constants/icons/VegetarianIcon';
 
 export const icons = {
-  user: <FontAwesome name="user" size={24} color="black" />,
-  data: <MaterialIcons name="data-usage" size={24} color="black" />,
-  analytics: <MaterialIcons name="analytics" size={24} color="black" />,
-  contact: <MaterialIcons name="connect-without-contact" size={24} color="black" />,
-  coffee: <FontAwesome name="coffee" size={24} color="black" />,
-  terms: <MaterialIcons name="description" size={24} color="black" />,
-  privacy: <MaterialIcons name="privacy-tip" size={24} color="black" />,
+  user: <FontAwesome name="user" size={32} color="black" />,
+  data: <MaterialIcons name="data-usage" size={32} color="black" />,
+  analytics: <MaterialIcons name="analytics" size={32} color="black" />,
+  contact: <MaterialIcons name="connect-without-contact" size={32} color="black" />,
+  coffee: <FontAwesome name="coffee" size={32} color="black" />,
+  terms: <MaterialIcons name="description" size={32} color="black" />,
+  privacy: <MaterialIcons name="privacy-tip" size={32} color="black" />,
   meatIcon: <MeatIcon width={32} height={32} />,
   vegetarianIcon: <VegetarianIcon width={32} height={32} />,
   veganIcon: <VeganIcon width={32} height={32} />,
